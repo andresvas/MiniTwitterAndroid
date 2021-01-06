4f8c6437d7edc43ff25d52eda4de2a270700c182
Andres Felipe Vasquez Saldarriaga <avasquez@todo1.com>
2021-01-06 18:42:24 -0500

    custom toolbar and icons from menu navigation

--- app/src/main/java/com/todo/bc/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/todo/bc/minitwitter/ui/DashboardActivity.java
@@ -1,18 +1,12 @@
 package com.todo.bc.minitwitter.ui;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.todo.bc.minitwitter.R;
-import com.todo.bc.minitwitter.common.Constants;
-import com.todo.bc.minitwitter.common.SharePrefencesManager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -21,14 +15,17 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        getSupportActionBar().hide();
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_tweets_like, R.id.navigation_profile)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupWithNavController(navView, navController);
 
 
 
